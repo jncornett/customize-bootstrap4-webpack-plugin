e415ce400c50d1865be56e9bb325a33dfcdc9f5c
Joel Cornett <joecorne@amazon.com>
2017-08-27 17:00:01 -0400

    add source add

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ class CustomizeBootstrap4WebpackPlugin {
   }
 
   apply(compiler) {
+    compiler.plugin('compilation', compilation => {
+      compilation.mainTemplate.plugin('render-with-entry', (source, chunk) => {
+        source.add('\n// Added this line!\n');
+      })
+    });
+    
     compiler.apply(new webpack.ProvidePlugin({
       // required for bootstrap
       jQuery: 'jquery',
       Popper: 'popper.js'
-    }))
+    }));
   }
 }
 
